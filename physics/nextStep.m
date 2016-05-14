ab700a4d9b717731a4a206b9fd6a86c3907d21c7
obama <baral.oh16@gmail.com>
2016-05-14 11:31:29 +0300

    idk

---
+++ physics/nextStep.m
@@ -0,0 +1,13 @@
+function [ new_car, new_velocity] = nextStep(car, wheel_angle, velocity, acceleration, dt)
+
+    new_velocity = velocity + acceleration * dt;
+    if wheel_angle < 0
+        
+    elseif wheel_angle > 0
+        
+    end
+    
+    
+
+end
+

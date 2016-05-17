6aaf58f98cdb993e4318126dcd2e6f83ffd39203
unknown <joni.rapoport@gmail.com>
2016-05-17 13:56:06 +0300

    commitLate

--- GeneticAlgorithm/RunEx1.m
+++
@@ -1,5 +0,0 @@
-load PermanentConditionsForFitness;
-conditions = struct('blastrad', blastrad, 'gravity', gravity, 'LandSky', LandSky, 'simulationdelta', simulationdelta, 'tmax', tmax, 'wind', wind, 'xEnemy', xEnemy, 'xTank', xTank, 'yEnemy', yEnemy, 'yTank', yTank);
-%conditions = struct('blastrad', 20, 'gravity', 300, 'LandSky', LandSky, 'simulationdelta', simulationdelta, 'tmax', tmax, 'wind', 200, 'xEnemy', xEnemy, 'xTank', xTank, 'yEnemy', yEnemy, 'yTank', yTank);
-scorchExGen(20,0.1,0.5,0.7,conditions);
-clear;
--- GeneticAlgorithm/enemyFunc.m
+++
@@ -1,8 +0,0 @@
-function [ curr ] = enemyFunc( prev, me )
-if isempty(prev)
-    curr = 0;
-else
-    curr = prev(end);
-end
-end
-

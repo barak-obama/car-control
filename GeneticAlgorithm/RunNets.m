1e0b5316e220826c94c3754e3ebb29d0f4b8457a
obama <baral.oh16@gmail.com>
2016-05-18 01:18:03 +0300

    stage commit

--- GeneticAlgorithm/RunNets.m
+++ GeneticAlgorithm/RunNets.m
@@ -1,7 +1,6 @@
 global dt
 dt = 0.1;
 DefaultMap;
-<<<<<<< HEAD
 netSize = [10];
 numStepsSim = 50;
 numGenerations = 50;
@@ -9,16 +8,7 @@ chromoNum = 10;
 elitePerc = 0.3;
 crossPerc = 0.7;
 mutPerc = 0.01;
-=======
 pause;
-netSize = [20];
-numStepsSim = 100;
-numGenerations = 50;
-chromoNum = 15;
-elitePerc = 0.7;
-crossPerc = 0.01;
-mutPerc = 0.003;
->>>>>>> 0179593d8083010e5469550da00a9869152c098e
 inL = length(getreality(str.cars{1}));
 [chromoLength, chromoIndex] = GenerateChromo(inL,2,netSize);
 bestChromo = NeuralNetWeights(str,chromoLength,chromoIndex,netSize,...

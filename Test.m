49d14b2bd0df696c71b84678b0b1ead3970f6ec5
obama <baral.oh16@gmail.com>
2016-05-21 16:31:24 +0300

    aaded test file, it creates a world without obstecalt

--- Test.m
+++ Test.m
@@ -1,8 +1,26 @@
 str = GenerateCarControlMap(50,50,4.7,2.1,20,1);
 
 str.obstacles = {};
-
 car = str.cars{1};
-
 car.x = 25;
 car.y = 25;
+car.angle = -pi/4;
+car.v = 1;
+
+[~, car.sensorData, car.points] = SensorData(car, str);
+str.cars{1} = car;
+
+displayCarControlMap(str, 0);
+
+dt = 0.01;
+
+canContinue = 1;
+
+while canContinue
+    [ str, canContinue ] = updatereality( str, str.cars{1}, 0, 1, dt, pi / 5  );
+    hold off
+    displayCarControlMap(str, 0);
+    pause(1);
+end
+    
+

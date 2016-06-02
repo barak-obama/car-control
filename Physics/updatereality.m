689f5ef457e1dbdd48af3afba61d7cfd5988b68e
obama <baral.oh16@gmail.com>
2016-06-02 19:10:34 +0300

    vudsbvos

--- Physics/updatereality.m
+++ Physics/updatereality.m
@@ -14,22 +14,46 @@ function [ str, canContinue ] = updatereality( str, car, a, wheel_angle, dt, max
         else
             b = back + abs(r) * [0 1; -1 0] * b;
         end
+        
+
+        plot(b(1), b(2), 'ob');
+        hold on;
+        
         theta = car.v * dt / r;
         new_end = rotateBy(back, theta, b);
-        p_angle = car.angle;
-        car.angle = 0;
-        if b(2) ~= new_end(2)
-            car.angle = pi / 2 - atan((new_end(1) - b(1)) / (b(2) - new_end(2)));
-            car.angle = car.angle/abs(car.angle)*mod(car.angle,2*pi);
-            if abs(p_angle - car.angle) > abs(p_angle - pi - car.angle)
-                car.angle = pi + car.angle;
-            elseif abs(p_angle - car.angle) > abs(p_angle + pi - car.angle)
-                car.angle = car.angle - pi;
-            end
-        else
-            display('should jump');
-        end
-        car.angle = roundArg(car.angle);
+        
+        m1 = -(new_end(1) - b(1))/(new_end(2) - b(2));
+        m2 = -(back(1) - b(1))/(back(2) - b(2));
+        angle = atan(abs((m1-m2)/(1-m1*m2)));
+        car.angle = car.angle - sign(r) * angle;
+        
+%         p_angle = car.angle;
+%         if b(2) ~= new_end(2)
+%         	car.angle = roundArg(pi / 2 - atan((new_end(1) - b(1)) / (b(2) - new_end(2))));
+%             if abs(p_angle - car.angle) > abs(p_angle - pi - car.angle)
+%                 car.angle = pi + car.angle;
+%         	elseif abs(p_angle - car.angle) > abs(p_angle + pi - car.angle)
+%             	car.angle = car.angle - pi;
+%             end
+%         else
+%             display('inside');
+%             if r > 0
+%                 if b(1)>new_end(1)
+%                     car.angle = 0;
+%                 else
+%                     car.angle = pi;
+%                 end
+%             else
+%                 if b(1)<new_end(1)
+%                     car.angle = 0;
+%                 else
+%                     car.angle = pi;
+%                 end
+%             end
+%         end
+        
+        
+
         car.x = (car.length / 2) * cos(pi/2 - car.angle) + new_end(1);
         car.y = (car.length / 2) * sin(pi/2 - car.angle) + new_end(2);
     else
--- Test.m
+++ Test.m
@@ -4,8 +4,10 @@ str.obstacles = {};
 car = str.cars{1};
 car.x = 25;
 car.y = 25;
+car.fx = 0;
+car.fy = 0;
 car.angle = -pi/4;
-car.v = 1;
+car.v = 6;
 
 [~, car.sensorData, car.points] = SensorData(car, str);
 str.cars{1} = car;
@@ -17,10 +19,10 @@ dt = 0.01;
 canContinue = 1;
 
 while canContinue
-    [ str, canContinue ] = updatereality( str, str.cars{1}, 0, 1, dt, pi / 5  );
+    [ str, canContinue ] = updatereality( str, str.cars{1}, 0, 1, dt, -pi/5  );
     hold off
     displayCarControlMap(str, 0);
-    pause(1);
+    pause(dt);
 end
     
 
